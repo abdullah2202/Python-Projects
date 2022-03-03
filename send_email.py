b585ca7a2d7af52417dab85e711f5cda4012429f
abdullah2202 <abdullah_22@hotmail.com>
2022-03-03 11:59:03 +0000

    Sending email working

--- send_email.py
+++ send_email.py
@@ -1,3 +1,22 @@
 from creds import email
+import smtplib, ssl
 
-print(email.username)
+email_username = email.username
+email_password = email.password
+
+smpt_address = "smtp.gmail.com"
+smpt_port = 465
+
+message = "This is a test message being sent"
+subject = "Test message send from PY"
+
+to_send = ssl.create_default_context()
+
+message_to_send = "Subject: {subject} \n\n {message}".format(
+    subject = subject,
+    message = message,
+)
+
+with smtplib.SMTP_SSL(smpt_address, smpt_port, context=to_send) as e:
+    e.login(email_username, email_password)
+    e.sendmail("Test",email_username,message_to_send)
