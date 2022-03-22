7c8f317796cfdbb331e365884dcb699e91f5f85c
abdullah2202 <abdullah_22@hotmail.com>
2022-03-22 12:45:43 +0000

    Working demo of mongodb interaction

--- pymonogo_test.py
+++ pymonogo_test.py
@@ -1,18 +1,17 @@
 import pymongo
 from creds import mongo_cred
 
-# myClient = pymongo.MongoClient("mongodb+srv://testuser:@cluster0.lbyme.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-
-
-myClient = pymongo.MongoClient(f"mongodb+srv://testuser:PdvuIGdByw5aOfQ6@cluster0.lbyme.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+myClient = pymongo.MongoClient(f"mongodb+srv://testuser:{mongo_cred.test_password}@cluster0.lbyme.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 myDB = myClient["sample_supplies"]
 myCol = myDB["sales"]
+
+# Show orders from Denver only
 query = {"storeLocation" : "Denver"}
 myRes = myCol.find(query)
 
-# Only print orders with more than 9 items
+# Only print orders with more than 5 items
 for x in myRes:
-    if(len(x["items"]) > 9):
+    if(len(x["items"]) > 5):
         print(x["saleDate"],", Items:",len(x["items"]))
 
 
