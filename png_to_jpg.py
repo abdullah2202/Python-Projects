08b62edd9383d66122b6f6c6893ddaf9edc6f083
abdullah2202 <abdullah_22@hotmail.com>
2022-07-04 18:44:37 +0100

    Added tinfy api

--- png_to_jpg.py
+++ png_to_jpg.py
@@ -1,12 +1,16 @@
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image
+from creds import tinfiy_api
+import tinify
+
+tinify.key = tinfiy_api.tf_api
 
 root = tk.Tk()
 canvas1 = tk.Canvas(root, width=300, height=250, bg='azure3', relief='raised')
 canvas1.pack()
 
-label1 = tk.Label(root, text="Image Converter", bg='azure3')
+label1 = tk.Label(root, text="Image Converter with Tinify", bg='azure3', wraplength=250)
 label1.config(font=('helvetica', 20))
 canvas1.create_window(150, 60, window=label1)
 
@@ -15,7 +19,7 @@ def getPNG():
     import_file_path = filedialog.askopenfilename()
     im1 = Image.open(import_file_path)
 
-browse_png = tk.Button(text="Select PNG file", command=getPNG, bg="royalblue", fg='white', font=('helvetica', 12, 'bold'))
+browse_png = tk.Button(text="Select Image file", command=getPNG, bg="royalblue", fg='white', font=('helvetica', 12, 'bold'))
 canvas1.create_window(150, 130, window=browse_png)
 
 def convert():
