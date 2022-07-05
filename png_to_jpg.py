572321ea272ebbf1a97c4c815370261c570af250
abdullah2202 <abdullah_22@hotmail.com>
2022-07-05 09:52:33 +0100

    Added webp

--- png_to_jpg.py
+++ png_to_jpg.py
@@ -5,8 +5,7 @@ from creds import tinfiy_api
 import tinify
 
 tinify.key = tinfiy_api.tf_api
-
-
+filetypes = [('PNG','*.png'),('JPG','*.jpg'),('WEBP','*.webp')]
 
 root = tk.Tk()
 canvas1 = tk.Canvas(root, width=300, height=250, bg='azure3', relief='raised')
@@ -16,6 +15,11 @@ label1 = tk.Label(root, text="Image Converter with Tinify", bg='azure3', wraplen
 label1.config(font=('helvetica', 20))
 canvas1.create_window(150, 60, window=label1)
 
+def convert():
+    global source
+    export_file_path = filedialog.asksaveasfilename(defaultextension='.png', filetypes=filetypes)
+    source.to_file(export_file_path)
+
 def getPNG():
     global source
     import_file_path = filedialog.askopenfilename()
@@ -24,12 +28,6 @@ def getPNG():
 browse_png = tk.Button(text="Select Image file", command=getPNG, bg="royalblue", fg='white', font=('helvetica', 12, 'bold'))
 canvas1.create_window(150, 130, window=browse_png)
 
-def convert():
-    global source
-    export_file_path = filedialog.asksaveasfilename(defaultextension='.png')
-    source.to_file(export_file_path)
-
-
-saveasbutton = tk.Button(text="Tinify to PNG", command=convert, bg='royalblue', fg='white', font=('helvetica', 12, 'bold'))
+saveasbutton = tk.Button(text="Tinify to PNG/JPG/WebP", command=convert, bg='royalblue', fg='white', font=('helvetica', 12, 'bold'))
 canvas1.create_window(150, 180, window=saveasbutton)
 root.mainloop()
